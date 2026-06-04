0d2a6a1e25bf93b6cdc61650b8f982519fcdf6a5
Sheikh Hamid <hamidsheikh858@gmail.com>
2026-06-04 16:26:26 +0530

    inditail commit

---
+++ src/test/java/com/aswesomeQA/test1/Test1.java
@@ -0,0 +1,26 @@
+package com.aswesomeQA.test1;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+    @Test
+    public void addition() {
+        System.out.println("Addition");
+    }
+
+    @Test
+    public void subtraction() {
+        System.out.println("Subtraction");
+    }
+
+    @Test
+    public void multiplication() {
+        System.out.println("Multiplication");
+    }
+
+    @Test
+    public void division() {
+        System.out.println("Division");
+    }
+}
